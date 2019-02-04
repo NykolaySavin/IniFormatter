e03da98537b9a167f66376d215b84834684d3e64
molodoytrezzini <savin171099@gmail.com>
2019-02-04 14:10:16 +0200

    Добавьте файлы проекта.

---
+++ IniFormatter/RootObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniFormatter
+{
+    public class Action
+    {
+        public int firstSymbolIndexInT { get; set; }
+        public int secondSymbolIndexInT { get; set; }
+        public int firstSymbolIndexInC { get; set; }
+        public int secondSymbolIndexInC { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string file { get; set; }
+        public List<int> numberOfDigits { get; set; }
+        public List<Action> actions { get; set; }
+    }
+}
